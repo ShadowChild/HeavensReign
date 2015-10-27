8c2c916bd2cb2592bbf2a825ea15d7b3c0362f0b
ShadowChild <shadychild12@gmail.com>
2015-10-27 20:26:59 +0000

    Learning SilenceEngine, added ShadowCommon

---
+++ SetupSilenceEngine.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+:buildEngine
+
+:copyLibs
+
---
+++ src/main/java/io/github/shadowchild/vdgame/Main.java
@@ -0,0 +1,47 @@
+package io.github.shadowchild.vdgame;
+
+import com.shc.silenceengine.core.Display;
+import com.shc.silenceengine.core.Game;
+import com.shc.silenceengine.graphics.Batcher;
+
+/**
+ * Created by Zach Piddock on 27/10/2015.
+ */
+public class Main extends Game {
+
+    public static final int HEIGHT = 1080;
+    public static final int WIDTH = (HEIGHT / 16) * 9;
+
+    // Initialize the resources
+    public void init() {
+
+        Display.setTitle("Ashleigh's Valentines Day Game");
+        Display.setSize(WIDTH, HEIGHT);
+        Display.setFullScreen(true);
+    }
+
+    // Update game logic
+    public void update(float delta) {
+
+    }
+
+    // Render to screen
+    public void render(float delta, Batcher batcher) {
+
+    }
+
+    // Handle window resize event
+    public void resize() {
+
+    }
+
+    // Dispose the resources
+    public void dispose() {
+
+    }
+
+    public static void main(String... args) {
+
+        new Main().start();
+    }
+}
