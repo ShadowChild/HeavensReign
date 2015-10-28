2be6d89ee2493279d0d137bd57ae256b69366ae2
ShadowChild <shadychild12@gmail.com>
2015-10-28 11:40:52 +0000

    Initial batch setup script

--- SetupSilenceEngine.bat
+++ SetupSilenceEngine.bat
@@ -1,6 +1,18 @@
 @ECHO OFF
 
+:setup
+set currentDir=%CD%
+set silenceDir=%currentDir%\SilenceEngine
+set mainDir=%silenceDir%\..\
+
 :buildEngine
+echo Copying Files...
+echo f | xcopy /y %currentDir%\setup.bat %currentDir%\SilenceEngine\setup.bat
+cd %silenceDir%
+call setup.bat
 
 :copyLibs
+echo Copying Libraries...
+echo f | xcopy /y %silenceDir%\build\libs\SilenceEngine.jar %mainDir%\libs\SilenceEngine.jar
 
+pause
---
+++ setup.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+call gradlew.bat clean build javadoc
+pause
