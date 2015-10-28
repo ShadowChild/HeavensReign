cfddee2b825b430a5c887ca17e27032d09b859e3
ShadowChild <shadychild12@gmail.com>
2015-10-28 12:39:30 +0000

    Copies the LWJGL libs now

--- SetupSilenceEngine.bat
+++ SetupSilenceEngine.bat
@@ -13,6 +13,7 @@ call setup.bat
 
 :copyLibs
 echo Copying Libraries...
-echo f | xcopy /y %silenceDir%\build\libs\SilenceEngine.jar %mainDir%\libs\SilenceEngine.jar
+echo f | xcopy /y %silenceDir%\build\libs\SilenceEngine.jar %mainDir%\libs\SilenceEngine\SilenceEngine.jar
+robocopy %silenceDir%\libs\ %mainDir%\libs\LWJGL\
 
 pause
