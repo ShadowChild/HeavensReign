5cb43520140c99fc6bef05a7d6886d05e1e33359
ShadowChild <ShadowChild@users.noreply.github.com>
2016-01-27 12:50:43 +0000

    Rewrote the travis_install script
    Not quite finished, but should remove unnecessary copying of libs

--- Tools/travis_install.sh
+++ Tools/travis_install.sh
@@ -1,17 +1,37 @@
 #!/usr/bin/env bash
 
-cd ./ShadowCommon
-mvn clean install
-cd ../SilenceEngine
-gradle clean build javadoc
-cd ..
-mkdir ./libs
-mkdir ./libs/SilenceEngine
-mkdir ./libs/LWJGL
-mkdir ./libs/ShadowCommon
-mkdir ./libs/ShadowCommon/core
-mkdir ./libs/ShadowCommon/opengl
-cp ./SilenceEngine/build/libs/SilenceEngine.jar ./libs/SilenceEngine/SilenceEngine.jar
-rsync ./SilenceEngine/libs ./libs/LWJGL
-rsync ./ShadowCommon/common-core ./libs/ShadowCommon/core
-rsync ./ShadowCommon/common-opengl ./libs/ShadowCommon/opengl
+function installSC() {
+  
+  cd ./ShadowCommon
+  mvn clean install
+  cd ..
+}
+
+function installSE() {
+  
+  cd ./SilenceEngine
+  gradle clean build javadoc
+  #mvnInstallSE()
+  cd ..
+}
+
+function mvnInstallSE() {
+  
+  mvn install:install-file -Dfile=./build/libs/SilenceEngine.jar -DgroupId=com.goharsha -DartifactId=silenceengine -Dversion=0.4.1 -Dpackaging=jar
+}
+
+function makeDirs() {
+  
+  mkdir ./libs
+  mkdir ./libs/SilenceEngine
+}
+
+function copyLibs() {
+  
+  cp ./SilenceEngine/build/libs/SilenceEngine.jar ./libs/SilenceEngine/SilenceEngine.jar  
+}
+
+installSC()
+installSE()
+makeDirs()
+copyLibs()
